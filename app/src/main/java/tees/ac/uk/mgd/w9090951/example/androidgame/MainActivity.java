c1d8eed85cc82f435456f8162f69141cbfd22d1b
Islam Dallali <islamjdallali@gmail.com>
2022-10-11 14:37:54 +0100

    Buttons launch new activity

--- app/src/main/java/tees/ac/uk/mgd/w9090951/example/androidgame/MainActivity.java
+++ app/src/main/java/tees/ac/uk/mgd/w9090951/example/androidgame/MainActivity.java
@@ -2,7 +2,9 @@ package tees.ac.uk.mgd.w9090951.example.androidgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void LaunchGameActivity(View view)
+    {
+        Intent intent = new Intent(this,GameActivity.class);
+        startActivity(intent);
     }
 }
