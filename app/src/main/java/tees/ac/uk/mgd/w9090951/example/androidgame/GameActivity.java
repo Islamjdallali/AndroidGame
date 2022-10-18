4e17148d4ae026a36a1b273fa93ee9a3d29d7a0f
Islam Dallali <islamjdallali@gmail.com>
2022-10-18 13:52:30 +0100

    Added game thread and onpause and onresume methods

--- app/src/main/java/tees/ac/uk/mgd/w9090951/example/androidgame/GameActivity.java
+++ app/src/main/java/tees/ac/uk/mgd/w9090951/example/androidgame/GameActivity.java
@@ -15,4 +15,18 @@ public class GameActivity extends AppCompatActivity {
         gameView = new GameView(this);
         setContentView(gameView);
     }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        gameView.resume();
+    }
+
+    @Override
+    protected  void onPause()
+    {
+        super.onPause();
+        gameView.pause();
+    }
 }
--- app/src/main/java/tees/ac/uk/mgd/w9090951/example/androidgame/GameView.java
+++ app/src/main/java/tees/ac/uk/mgd/w9090951/example/androidgame/GameView.java
@@ -1,9 +1,13 @@
 package tees.ac.uk.mgd.w9090951.example.androidgame;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.SurfaceView;
 
 public class GameView extends SurfaceView implements Runnable {
+
+    private volatile boolean playing;
+    private Thread gameThread;
 
     public GameView(Context context) {
         super(context);
@@ -11,6 +15,24 @@ public class GameView extends SurfaceView implements Runnable {
 
     @Override
     public void run() {
+    }
 
+    public void resume()
+    {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    public void pause()
+    {
+        playing = false;
+        try
+        {
+         gameThread.join();
+        }
+        catch (InterruptedException e)
+        {
+            Log.e("GameView", "Interrupted");
+        }
     }
 }
