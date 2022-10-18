affaffd0c945bb976c368b211aba156be4204a5e
Islam Dallali <islamjdallali@gmail.com>
2022-10-18 13:40:19 +0100

    Set Gameview

---
+++ app/src/main/java/tees/ac/uk/mgd/w9090951/example/androidgame/GameView.java
@@ -0,0 +1,16 @@
+package tees.ac.uk.mgd.w9090951.example.androidgame;
+
+import android.content.Context;
+import android.view.SurfaceView;
+
+public class GameView extends SurfaceView implements Runnable {
+
+    public GameView(Context context) {
+        super(context);
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
