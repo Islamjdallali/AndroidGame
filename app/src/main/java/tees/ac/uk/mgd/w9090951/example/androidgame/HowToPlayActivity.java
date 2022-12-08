57ff48dd595a08a6a07413d7f4944ac22d618a1a
IslamDallali <islamjdallali@gmail.com>
2022-12-08 00:21:30 +0000

    added howtoplay section

--- app/src/main/java/tees/ac/uk/mgd/w9090951/example/androidgame/HowToPlayActivity.java
+++ app/src/main/java/tees/ac/uk/mgd/w9090951/example/androidgame/HowToPlayActivity.java
@@ -2,7 +2,9 @@ package tees.ac.uk.mgd.w9090951.example.androidgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HowToPlayActivity extends AppCompatActivity {
 
@@ -11,5 +13,10 @@ public class HowToPlayActivity extends AppCompatActivity {
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_how_to_play);
+    }
+
+    public void EndActivity(View view)
+    {
+        finish();
     }
 }
